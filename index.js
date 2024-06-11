c56159afc9e5d483912b07ecd096f517b4811872
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-06-11 19:00:10 +0200

    Error handling was added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import 'dotenv/config';
-import { Bot } from 'grammy';
+import { Bot, GrammyError, HttpError } from 'grammy';
 
 const bot = new Bot(process.env.BOT_API_KEY);
 
-bot.command('start', (ctx) => {
-  ctx.reply('Привет! Я бот-фронтендер!');
+bot.command('start', async (ctx) => {
+  await ctx.reply(
+    'Привет! Я бот-фронтендер! 🌚\nЯ помогу тебе подготовиться к собеседованию по HTML, CSS, JavaScript и React!'
+  );
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
 });
 
 bot.start();
